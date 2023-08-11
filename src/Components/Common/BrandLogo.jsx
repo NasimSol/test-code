218df65148e0b61e8da50341e9ab9a631ce26ac5
NasimSoleymani <nasim.soleymanid@gmail.com>
2023-08-11 16:03:23 +0330

    Adding fiber logo

---
+++ src/Components/Common/BrandLogo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Logo = styled.p`
+  font-weight: 700;
+  font-size: 1.5rem;
+`;
+
+const BrandLogo = () => {
+  return <Logo>Fiber</Logo>;
+};
+
+export default BrandLogo;
